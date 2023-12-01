b9ba07f2b911f56fb1149bd64f3e7b1d6ae25607
zahir <zahir@capia.no>
2023-12-01 10:13:23 +0100

    deploy test

--- script.sh
+++ script.sh
@@ -4,10 +4,12 @@ echo "Backup script for cvat triggered at $(date)"
 # Get the current date in the format YYYY-MM-DD
 current_date=$(date +%Y%m%d)
 backup_dir="/volumes/cvat_new/backup/$current_date"
-# docker compose stop
-mkdir "$backup_dir"
+mkdir -p "$backup_dir"
+
+docker compose stop
+
 docker run --rm --name temp_backup --volumes-from cvat_db -v $(pwd)/"$backup_dir":/backup ubuntu tar -czvf /backup/cvat_db.tar.gz /var/lib/postgresql/data
 docker run --rm --name temp_backup --volumes-from cvat_server -v $(pwd)/"$backup_dir":/backup ubuntu tar -czvf /backup/cvat_data.tar.gz /home/django/data
 
-# docker compose up -d
+docker compose up -d
 echo "Backup script for cvat completed at $(date)"
